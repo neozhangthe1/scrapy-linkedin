6bd7bc4fe73ecaf579836636388f3fb10e35ac65
Yutao Zhang <stack@live.cn>
2013-08-12 19:48:47 +0800

    add videolectures

---
+++ crawler/videolectures/settings.py
@@ -0,0 +1,43 @@
+# Scrapy settings for linkedin project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+import os
+
+BOT_NAME = 'videolectures'
+
+SPIDER_MODULES = [BOT_NAME+'.spiders']
+NEWSPIDER_MODULE = BOT_NAME+'.spiders'
+
+DOWNLOADER_MIDDLEWARES = {
+    BOT_NAME+'.middleware.CustomHttpProxyMiddleware': 543,
+    BOT_NAME+'videolectures.middleware.CustomUserAgentMiddleware': 545,
+}
+
+########### Item pipeline
+ITEM_PIPELINES = [
+                  BOT_NAME+".pipelines.MongoDBPipeline",
+]
+
+MONGODB_SERVER = '10.1.1.111'
+MONGODB_PORT = 12345
+MONGODB_DB = BOT_NAME
+MONGODB_COLLECTION = 'person_profiles'
+MONGODB_UNIQ_KEY = '_id'
+###########
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'linkedin (+http://www.yourdomain.com)'
+
+# Enable auto throttle
+AUTOTHROTTLE_ENABLED = True
+
+COOKIES_ENABLED = False
+
+# Set your own download folder
+DOWNLOAD_FILE_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), "download_file")
+
+
---
+++ crawler/videolectures/spiders/VideolecturesSpider.py
@@ -0,0 +1,127 @@
+from scrapy.selector import HtmlXPathSelector
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.http import Request
+from scrapy import log
+from linkedin.items import LinkedinItem, PersonProfileItem
+from os import path
+from linkedin.parser.HtmlParser import HtmlParser
+import os
+import urllib
+import string
+from bs4 import UnicodeDammit
+from linkedin.db import MongoDBClient
+
+class VideolecturesspiderSpider(CrawlSpider):
+    name = 'VideolecturesSpider'
+    allowed_domains = ['videolectures.net']
+    start_urls = [ "http://videolectures.net/site/list/authors/?page=%s" % s for s in range(1,22)]
+
+    rules = (
+        #Rule(SgmlLinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+    )
+
+    def __init__(self):
+        pass
+        
+    def parse(self, response):
+        """
+        default parse method, rule is not useful now
+        """
+        response = response.replace(url=HtmlParser.remove_url_parameter(response.url))
+        hxs = HtmlXPathSelector(response)
+        index_level = self.determine_level(response)
+        if index_level == 1:
+            #self.save_to_file_system(index_level, response)
+            relative_urls = self.get_follow_links(index_level, hxs)
+            if relative_urls is not None:
+                for url in relative_urls:
+                    yield Request(url, callback=self.parse)
+        elif index_level == 2:
+            personProfile = HtmlParser.extract_videolectures_profile(hxs)
+            videolectures_id = self.get_videolectures_id(response.url)
+            #videolectures_id = UnicodeDammit(urllib.unquote_plus(linkedin_id)).markup
+            if linkedin_id:
+                personProfile['_id'] = videolectures_id
+                personProfile['url'] = UnicodeDammit(response.url).markup
+                yield personProfile
+    
+    def determine_level(self, response):
+        """
+        determine the index level of current response, so we can decide wether to continue crawl or not.
+        level 1: site/list/authors/
+        level 2: person page
+        level 3: lecture page
+        level 4: org
+        level 5: conference
+        """
+        import re
+        url = response.url
+        if re.match(".+/site/list/authors", url):
+            return 1
+        else:
+            return 2
+        #elif re.match(".+/[A-Z]\d+.html", url):
+        #    return 2
+        #elif re.match(".+/people/[a-zA-Z0-9-]+.html", url):
+        #    return 3
+        #elif re.match(".+/pub/dir/.+", url):
+        #    return 4
+        #elif re.match(".+/search/._", url):
+        #    return 4
+        #elif re.match(".+/pub/.+", url):
+        #    return 5
+        return None
+    
+    def save_to_file_system(self, level, response):
+        """
+        save the response to related folder
+        """
+        if level in [1, 2, 3, 4, 5]:
+            fileName = self.get_clean_file_name(level, response)
+            if fileName is None:
+                return
+            
+            fn = path.join(self.settings["DOWNLOAD_FILE_FOLDER"], str(level), fileName)
+            self.create_path_if_not_exist(fn)
+            if not path.exists(fn):
+                with open(fn, "w") as f:
+                    f.write(response.body)
+    
+    def get_clean_file_name(self, level, response):
+        """
+        generate unique linkedin id, now use the url
+        """
+        url = response.url
+        if level in [1, 2, 3]:
+            return url.split("/")[-1]
+        
+        videolectures_id = self.get_videolectures_id(url)
+        if videolectures_id:
+            return videolectures_id
+        return None
+        
+    def get_videolectures_id(self, url):
+        find_index = url.find("videolectures.net/")
+        if find_index >= 0:
+            return url[find_index:].replace('/', '')
+        return None
+        
+    def get_follow_links(self, level, hxs):
+        if level == 1:
+            relative_urls = hxs.select("//tr[@class='al_tint']/td/td/b/a/@href").extract()
+            relative_urls = ["http://videolectures.net" + x for x in relative_urls]
+        
+        #if level in [1, 2, 3]:
+        #    relative_urls = hxs.select("//ul[@class='directory']/li/a/@href").extract()
+            
+        #    return relative_urls
+        #elif level == 4:
+        #    relative_urls = relative_urls = hxs.select("//ol[@id='result-set']/li/h2/strong/a/@href").extract()
+        #    relative_urls = ["http://linkedin.com" + x for x in relative_urls]
+        return relative_urls
+
+    def create_path_if_not_exist(self, filePath):
+        if not path.exists(path.dirname(filePath)):
+            os.makedirs(path.dirname(filePath))
+            
