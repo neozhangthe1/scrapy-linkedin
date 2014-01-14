08d955012b4903ecf1b67bdac10041f3c2f312d8
neozhangthe1 <stack@live.cn>
2014-01-15 00:55:36 +0800

    added flickr spider

--- linkedin/linkedin/spiders/LinkedinSpider.py
+++ linkedin/linkedin/spiders/LinkedinSpider.py
@@ -1,30 +1,30 @@
-from scrapy.selector import HtmlXPathSelector
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
-from scrapy.contrib.spiders import CrawlSpider, Rule
-from scrapy.http import Request
-from scrapy import log
-from linkedin.items import LinkedinItem, PersonProfileItem
 from os import path
-from linkedin.parser.HtmlParser import HtmlParser
 import os
 import urllib
-import string
+
+from scrapy.selector import HtmlXPathSelector
+from scrapy.contrib.spiders import CrawlSpider
+from scrapy.http import Request
 from bs4 import UnicodeDammit
+
+from linkedin.items import LinkedinItem, PersonProfileItem
+from linkedin.parser.HtmlParser import HtmlParser
 from linkedin.db import MongoDBClient
+
 
 class LinkedinspiderSpider(CrawlSpider):
     name = 'LinkedinSpider'
     allowed_domains = ['linkedin.com']
-    start_urls = [ "http://www.linkedin.com/directory/people/%s.html" % s 
-                   for s in "abcdefghijklmnopqrstuvwxyz" ]
+    start_urls = ["http://www.linkedin.com/directory/people/%s.html" % s
+                  for s in "abcdefghijklmnopqrstuvwxyz"]
 
     rules = (
-        #Rule(SgmlLinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+    #Rule(SgmlLinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
     )
 
     def __init__(self):
         pass
-        
+
     def parse(self, response):
         """
         default parse method, rule is not useful now
@@ -46,7 +46,7 @@ class LinkedinspiderSpider(CrawlSpider):
                 personProfile['_id'] = linkedin_id
                 personProfile['url'] = UnicodeDammit(response.url).markup
                 yield personProfile
-    
+
     def determine_level(self, response):
         """
         determine the index level of current response, so we can decide wether to continue crawl or not.
@@ -57,6 +57,7 @@ class LinkedinspiderSpider(CrawlSpider):
         level 5: profile page
         """
         import re
+
         url = response.url
         if re.match(".+/[a-z]\.html", url):
             return 1
@@ -71,7 +72,7 @@ class LinkedinspiderSpider(CrawlSpider):
         elif re.match(".+/pub/.+", url):
             return 5
         return None
-    
+
     def save_to_file_system(self, level, response):
         """
         save the response to related folder
@@ -80,13 +81,13 @@ class LinkedinspiderSpider(CrawlSpider):
             fileName = self.get_clean_file_name(level, response)
             if fileName is None:
                 return
-            
+
             fn = path.join(self.settings["DOWNLOAD_FILE_FOLDER"], str(level), fileName)
             self.create_path_if_not_exist(fn)
             if not path.exists(fn):
                 with open(fn, "w") as f:
                     f.write(response.body)
-    
+
     def get_clean_file_name(self, level, response):
         """
         generate unique linkedin id, now use the url
@@ -94,18 +95,18 @@ class LinkedinspiderSpider(CrawlSpider):
         url = response.url
         if level in [1, 2, 3]:
             return url.split("/")[-1]
-        
+
         linkedin_id = self.get_linkedin_id(url)
         if linkedin_id:
             return linkedin_id
         return None
-        
+
     def get_linkedin_id(self, url):
         find_index = url.find("linkedin.com/")
         if find_index >= 0:
             return url[find_index + 13:].replace('/', '-')
         return None
-        
+
     def get_follow_links(self, level, hxs):
         if level in [1, 2, 3]:
             relative_urls = hxs.select("//ul[@class='directory']/li/a/@href").extract()
@@ -119,4 +120,3 @@ class LinkedinspiderSpider(CrawlSpider):
     def create_path_if_not_exist(self, filePath):
         if not path.exists(path.dirname(filePath)):
             os.makedirs(path.dirname(filePath))
-            
